8efd2e486a1329be91060d4cb1324622ba9b35ac
sisisin <azsisisin@gmail.com>
2016-01-16 13:18:04 +0900

    tsc and browserify.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+
 const tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('tsc', () => {
@@ -7,3 +10,12 @@ gulp.task('tsc', () => {
     .pipe(ts(tsProject))
     .pipe(gulp.dest('dist/'));
 });
+
+gulp.task('browserify', ['tsc'], () => {
+  return browserify({ entries: ['./dist/app.js']})
+    .bundle()
+    .pipe(source('./tmp/app.js'))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', ['browserify']);
--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,3 @@
+import {cnsl} from './mod';
 
-class Student {
-  fullname: string;
-  constructor(public firstname, public middleinitial, public lastname) {
-    this.fullname = firstname + ' ' + middleinitial + ' ' + lastname;
-  }
-}
-
-interface Person {
-  firstname: string;
-  lastname: string;
-}
-
-
-function greeter (person : Person) {
-  return 'Hello, ' + person.firstname + ' ' + person.lastname;
-}
-
-const user = new Student('Jane', 'M.', 'User');
-console.log(greeter(user));
-
-
+console.log(cnsl());
