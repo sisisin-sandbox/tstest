404c44629d7440d3f088fef491ab24cef1d2c7fc
sisisin <azsisisin@gmail.com>
2016-01-16 14:02:32 +0900

    use mocha

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 const gulp = require('gulp');
+const mocha = require('gulp-mocha');
 const browserify = require('browserify');
 const tsify = require('tsify');
 const source = require('vinyl-source-stream');
+
+const espowerTypescript = require('espower-typescript/guess');
 
 gulp.task('browserify', () => {
   return browserify({ entries: ['./src/app.ts']})
@@ -9,6 +12,14 @@ gulp.task('browserify', () => {
     .bundle()
     .pipe(source('./dist/app.js'))
     .pipe(gulp.dest('./'));
+});
+
+gulp.task('mocha', () => {
+  return gulp.src(['test/*.ts'])
+    .pipe(mocha({
+        reporter: 'nyan',
+        compilers: { ts: espowerTypescript }
+      }));
 });
 
 gulp.task('default', ['browserify']);
